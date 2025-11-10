d9084186376045759dcd5ef4780c5c98cafeb625
SNAILX <180862994+SNAILX808@users.noreply.github.com>
2025-11-11 00:57:39 +0100

    Move-refactor-raycast-functions-esx_lib

--- [core]/es_extended/client/compat.lua
+++ [core]/es_extended/client/compat.lua
@@ -1,1 +1,22 @@
 --All client-side functions outsourced from the Core to the lib will be stored here for compatability, e.g:
+
+ESX.Game.GetShapeTestResultSync = xLib.Raycast.GetShapeTestResult
+ESX.Game.RaycastScreen = xLib.Raycast.FromScreen
+ESX.Game.StartRaycasting = xLib.Raycast.Start
+ESX.Game.StopRaycasting = function(raycast)
+    if raycast and raycast.active then
+        raycast:Stop()
+    end
+end
+ESX.Game.IsRaycastActive = function(raycast)
+    if raycast and raycast.active then
+        return raycast:IsActive()
+    end
+    return false
+end
+ESX.Game.GetRaycastResult = function(raycast)
+    if raycast and raycast.active then
+        return raycast.result
+    end
+    return nil
+end
--- [core]/es_extended/client/functions.lua
+++ [core]/es_extended/client/functions.lua
@@ -736,27 +736,6 @@ function ESX.Game.IsSpawnPointClear(coords, maxDistance)
     return #ESX.Game.GetVehiclesInArea(coords, maxDistance) == 0
 end
 
----@param shape integer The shape to get the test result from
----@return boolean, table, table, integer, integer
-function ESX.Game.GetShapeTestResultSync(shape)
-	local handle, hit, coords, normal, material, entity
-	repeat
-        handle, hit, coords, normal, material, entity = GetShapeTestResultIncludingMaterial(shape)
-        Wait(0)
-	until handle ~= 1
-	return hit, coords, normal, material, entity
-end
-
----@param depth number The depth to raycast
----@vararg any The arguments to pass to the shape test
----@return table, boolean, table, table, integer, integer
-function ESX.Game.RaycastScreen(depth, ...)
-	local world, normal = GetWorldCoordFromScreenCoord(.5, .5)
-	local origin = world + normal
-	local target = world + normal * depth
-	return target, ESX.Game.GetShapeTestResultSync(StartShapeTestLosProbe(origin.x, origin.y, origin.z, target.x, target.y, target.z, ...))
-end
-
 ---@param entities table The entities to search through
 ---@param isPlayerEntities boolean Whether the entities are players
 ---@param coords? table | vector3 The coords to search from
